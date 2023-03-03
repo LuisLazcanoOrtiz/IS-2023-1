00fe237fa8ae39135e6fd3a4328473063d2798dd
LuisLazcanoOrtiz <luis.lazcano@uabc.edu.mx>
2023-03-03 11:58:09 -0800

    Primer subida

---
+++ Config/Config.php
@@ -0,0 +1,38 @@
+<?php
+const base_url = "http://localhost/AgendaWeb/";
+const host = "localhost";
+const user = "root";
+const pass = "";
+const db = "eventos";
+const charset = "charset=utf8";
+/* 
+Tabla de servicios
+Id_Servicio
+Nombre servicio
+Precio Servicio
+
+Eventos 
+id_evento
+Nombre Evento
+Hora
+Descripcion
+Servicio
+Cliente
+color
+
+Usuarios
+id_usuario
+usuario
+pass
+Nombre
+Ap_m
+Ap_p
+
+Clientes
+id cliente
+Nombre Cliente
+RFC
+
+*/
+?>
+
